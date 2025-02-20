f26ce2ed245b333d04fbced9adad6497ec209035
Ersan YAKIT <ersanyakit@yahoo.com.tr>
2025-02-20 11:56:37 +0800

    fixes

--- fixlogo.go
+++ fixlogo.go
@@ -37,8 +37,7 @@ type Tags struct{}
 
 func main() {
 	// Dosyadan JSON verisini oku
-
-	filePath := "./avax/index.json"
+	filePath := "./chiliz/index.json"
 	jsonData, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		fmt.Println("Dosya okuma hatası:", err)
@@ -59,7 +58,7 @@ func main() {
 	for i := range swapDefault.Tokens {
 
 		var ContractAddress = strings.ToLower(swapDefault.Tokens[i].Address)
-		var NewLogoURL = fmt.Sprintf("https://raw.githubusercontent.com/kewlexchange/assets/main/avax/tokens/%s/logo.svg", ContractAddress)
+		var NewLogoURL = fmt.Sprintf("https://raw.githubusercontent.com/kewlexchange/assets/main/chiliz/tokens/%s/logo.svg", ContractAddress)
 		swapDefault.Tokens[i].LogoURI = NewLogoURL
 	}
 
